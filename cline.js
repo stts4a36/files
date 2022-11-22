839f14210f241c8bc2a73b2028764c0cf2955c74
stts4a36 <tcyuenac@ust.hk>
2022-11-23 00:20:33 +0800

    Update cline.js

--- cline.js
+++ cline.js
@@ -2,7 +2,7 @@
 
 
 
-setTimeout(function(){document.location.href = "https://accedss.cityline.com/utsvInternet/L20NP22B/login?lang=TW/";},250);
+setTimeout(function(){document.location.href = "https://accss.cityline.com/utsvInternet/L20NP22B/login?lang=TW/";},250);
 
 
 
